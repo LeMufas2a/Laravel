76a579d4a9ad792296fd7acf2ef14c023fdd2202
LeMufas2a <codage.mufasa@gmail.com>
2021-04-28 16:25:18 +0200

    template exo 1

---
+++ laravel-template-exo-1/resources/js/app.js
@@ -0,0 +1,7 @@
+require('jquery/dist/jquery');
+require('jquery.easing/jquery.easing');
+require('./bootstrap');
+require('./jqBootstrapValidation');
+require('./agency');
+require('./contact_me');
+
---
+++ laravel-template-exo-1/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('content')
+@include('partials.header')
+@include('partials.services')
+@include('partials.portfolio')
+@include('partials.about')
+@include('partials.team')
+@include('partials.clients')
+@include('partials.contact')
+@include('partials.footer')
+@endsection
