0a37bdac5363c720a522a26280be5bad0b90a6d2
LeMufas2a <codage.mufasa@gmail.com>
2021-04-28 16:23:21 +0200

    route exo 4

---
+++ laravel-route-exo-4/resources/views/fruit.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href={{ asset("css/app.css") }}>
+</head>
+<body>
+    <header>
+        @include('navbar')
+
+    </header>
+    <ul>
+        @foreach ($fruits as $item)
+        <li>{{ $loop->index }} {{ $item }}</li>
+        @endforeach
+    </ul>
+    @include('footer')
+     <script  src={{  asset("js/app.js")  }}></script>
+</body>
+</html>
